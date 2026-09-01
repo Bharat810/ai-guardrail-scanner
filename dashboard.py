d62ccffea2639f09a361762342a6e65fb737cd76
Bharat Kumar Grover <bharatgrover49@gamil.com>
2026-09-01 07:21:30 +0530

    importing os in dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
 from PIL import Image
+import os
 
 # Use environment variable or default to local for development
 API_URL = st.secrets.get("API_URL", "http://127.0.0.1:8000")
